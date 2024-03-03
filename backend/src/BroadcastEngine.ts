0b00d07e53b6249746e06fa956eb17a03ab2b92f
Kyle Winn <kylealexanderwinn@gmail.com>
2024-03-03 03:22:42 +0000

    new data for vote

--- backend/src/BroadcastEngine.ts
+++ backend/src/BroadcastEngine.ts
@@ -2,9 +2,9 @@ import { voteTally } from "../../shared/sharedTypes";
 
 export class BroadcastEngine {
     broadcastWinningFilm(message: voteTally[]) {
-        for (const vote of message) {
-            console.log(vote.filmId, vote.votes);
-        }
+        // for (const vote of message) {
+        //     console.log(vote.filmId, vote.votes);
+        // }
     }
 
     broadcastSessionStarted(message: string) {
